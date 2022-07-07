fc18124e382652d17afe3cc077b1aabb45206848
Stepan <studiorain27@gmail.com>
2022-07-07 16:17:22 +0300

    Простой счётчик

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rxjs-learning';
+  counter: number = 0;
+  updatedAt?: number;
+
+  get canItBeDecreased(): boolean {
+    return this.counter <= 0;
+  }
+
+  public increment(): void {
+    this.updatedAt = Date.now();
+    this.counter++;
+  }
+
+  public decrement(): void  {
+    this.updatedAt = Date.now();
+    this.counter--;
+  }
+
+  public clear(): void {
+    this.updatedAt = Date.now();
+    this.counter = 0;
+  }
 }
